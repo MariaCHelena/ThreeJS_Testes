a1253a6daa1142e68cc2c96a25ba49b488b88430
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-06-06 19:57:23 -0300

    Renderizando objeto 3D importado

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
 
-//cena
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 100 );
 
-//criando uma esfera
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({
-    color: "#00FF83",
+const loader = new GLTFLoader();
+
+loader.load( '/kirara.glb', function(glb){
+    console.log(glb)
+    const root = glb.scene;
+    root.scale.set(45, 45, 45)
+
+    scene.add(root)
+
+}, function(xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function(error){
+    console.log(error)
 })
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
 //luz
-const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(10, 10, 10)
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(2, 2, 5)
 scene.add(light);
 
-//câmera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
-camera.position.z = 15;
-scene.add(camera);
+camera.position.z = 10;
+camera.position.x = 0;
+camera.position.y = 5;
 
-//Renderizador
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild( renderer.domElement );
-renderer.render(scene, camera)
+
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+
+animate()

2bc741e3556517a13ff91a5b8a81f6109942be15
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-06-07 00:53:19 -0300

    Criando cenário dinâmico

--- main.js
+++ main.js
@@ -1,28 +1,91 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 
-//cena
-const scene = new THREE.Scene();
+import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
 
-//criando uma esfera
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({
-    color: "#00FF83",
-})
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+class BasicWorldDemo {
+  constructor (){
+      this._Initialize();
+  }
 
-//luz
-const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(10, 10, 10)
-scene.add(light);
+  _Initialize() {
+      this._threejs = new THREE.WebGLRenderer({
+        antialias: true,
+      });
+      this._threejs.shadowMap.enabled = true;
+      this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
+      this._threejs.setPixelRatio(window.devicePixelRatio);
+      this._threejs.setSize(window.innerWidth, window.innerHeight);
 
-//câmera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
-camera.position.z = 15;
-scene.add(camera);
+      document.body.appendChild(this._threejs.domElement);
 
-//Renderizador
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild( renderer.domElement );
-renderer.render(scene, camera)
+      const fov = 60;
+      const aspect = 1920/1080;
+      const near = 1.0;
+      const far = 1000.0;
+      this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+      this._camera.position.set(75, 20, 0);
+
+      this._scene = new THREE.Scene();
+
+      let light = new THREE.DirectionalLight(0xFFFFFF);
+      light.position.set(20, 100, 10);
+      light.target.position.set(0, 0, 0);
+      light.castShadow = true;
+      light.shadow.bias = -0.001;
+      light.shadow.mapSize.width = 2048;
+      light.shadow.mapSize.height = 2048;
+      light.shadow.camera.near = 0.1;
+      light.shadow.camera.far = 500.0;
+      light.shadow.camera.near = 0.5;
+      light.shadow.camera.far = 500.0;
+      light.shadow.camera.left = 100;
+      light.shadow.camera.right = -100;
+      light.shadow.camera.top = 100;
+      light.shadow.camera.bottom = -100;
+      this._scene.add(light);
+
+      light = new THREE.AmbientLight(0xFFFFFF);
+      this._scene.add(light);
+
+      const controls = new OrbitControls(
+        this._camera, this._threejs.domElement);
+      controls.target.set(0, 20, 0);
+      controls.update();
+
+      const loader = new THREE.CubeTextureLoader();
+      const texture = loader.load([
+          './resources/posx.jpg',
+          './resources/negx.jpg',
+          './resources/posy.jpg',
+          './resources/negy.jpg',
+          './resources/posz.jpg',
+          './resources/negz.jpg',
+      ]);
+      this._scene.background = texture;
+
+      const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(100, 100, 1, 10),
+        new THREE.MeshStandardMaterial({
+          color: 0xFFFFFF,
+        }));
+      plane.castShadow = false;
+      plane.receiveShadow = true;
+      plane.rotation.x = -Math.PI / 2;
+      this._scene.add(plane);
+
+      this._RAF();
+  }
+
+  _RAF() {
+      requestAnimationFrame(() => {
+          this._threejs.render(this._scene, this._camera);
+          this._RAF();
+      })
+  }
+}
+
+let _APP = null;
+
+window.addEventListener('DOMContentLoaded', () => {
+  _APP = new BasicWorldDemo();
+});
